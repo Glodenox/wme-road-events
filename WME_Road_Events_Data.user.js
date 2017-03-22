19d2bcd380a18dc25239496509800c837006a8b3
Glodenox <github@tomputtemans.com>
2017-03-22 17:22:20 +0100

    Fixed translation strings (changed from en to en-US)

--- WME_Road_Events_Data.user.js
+++ WME_Road_Events_Data.user.js
@@ -3,7 +3,7 @@
 // @namespace   http://www.tomputtemans.com/
 // @description Retrieve and show road events
 // @include     /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor\/.*$/
-// @version     1.5
+// @version     1.6
 // @grant       none
 // ==/UserScript==
 
@@ -1024,7 +1024,7 @@
         }
       }
     };
-    strings.en_GB = strings.en;
+    strings.en_GB = strings['en-US'] = strings.en;
     I18n.availableLocales.forEach(function(locale) {
       if (I18n.translations[locale]) {
         I18n.translations[locale].road_events = strings[locale];
